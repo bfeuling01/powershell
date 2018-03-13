878b0f6644dfd2afafcb2bcb146e20f2cf0e5297
Bryan <bfeuling@gmail.com>
2018-03-13 12:41:57 -0500

    Created 1607 Win Update
    Downloads the most recent Windows Update for Win10.1607

---
+++ 1607WinUpdate.ps1
@@ -0,0 +1,7 @@
+Import-Module BitsTransfer
+$month = (Get-Date -Format M).Split(" ")[0]
+$search="https://www.google.com/search?q=itechtics+Windows+10+1607+$month+Cumulative+update+download"
+
+$downloadSite = (((Invoke-WebRequest -Uri $search).Links | Where {$_.href -match "www.itechtics.com"} | Select href -First 1).href).Substring(7)
+$downloadSite = ($downloadSite -split "&amp")[0]
+Start-BitsTransfer -Source ((Invoke-WebRequest -Uri $downloadSite -Verbose).Links | Where {$_.href -match "download.windowsupdate.com"} | Select href -First 1).href -Destination "<Destination>\1607$month.msu" -Verbose
